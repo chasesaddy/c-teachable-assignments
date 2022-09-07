352c91da367c976f8e38d9a56943a5d1f699a762
Saadh S <atextbooksituation@gmail.com>
2022-09-07 17:32:42 -0400

    Edits from Monday before meeting. Testing code had it working, but it’s funky between leetcode and Coachable. Will keep to Coachable only when adding tests

--- section_06_hashmap-double-ended/browser_history.py
+++ section_06_hashmap-double-ended/browser_history.py
@@ -4,133 +4,72 @@ class Node:
     self.prev = prev
     self.next = next
 
-class BrowserHistory(object):
+class BrowserHistory:
   # Initializes browser hisotry from new tab. 
   # By default the starting URL will be an empty string.
   def __init__(self, homepage): 
-      self.head = None
-      self.tail = None
-      self.visit( homepage )
+      self.deque = Node( homepage )
   
   # Returns the url of the current page.
   def current_page(self):
-      return self.url
+      return self.deque.val
   
   # Navigates to the "page" from the current page. 
   # Nagivating forward should overwrite all previous forward browser history.
   def visit(self, page): 
+      tmp = self.deque
       new_node = Node( page )
-      if self.head is not None:
-        self.add_to_front_of_tail( self.head )
-      self.head = new_node
+      self.deque.next = new_node      
+      self.deque = new_node
+      self.deque.prev = tmp
   
   # Navigates to the previous page visited.
   # After navigating return the URL of the current page.
   def go_back(self):
-      curr = self.tail
-      curr = curr.prev
-      if curr is None:
-          return
-      self.tail = curr
-      return curr.val
+      self.deque = self.deque.prev      
+      return self.deque.val
     
   # Navigates to the page ahead in browser history.
   # If there is no page ahead, do nothing.
   # After navigating return the URL of the current page.
   def go_forward(self):
-      curr = self.head
-      #   if curr.next is None:
-      #       return
-      curr = curr.next
-      if curr is None:
-          return
-      self.head = curr
-      return curr.val
+      if self.deque.next is not None:
+        self.deque = self.deque.next
+        return self.deque.val
   
   # Navigates backwards N pages in browser history.
   # If there are not N pages behind, then return the new tab URL which is an empty string.
   # After navigating return the URL of the current page.
   def back(self, N):
-      if self.tail is None:
-          return
-      curr = self.tail
       for i in range( N ):
-          if curr.prev is None:
+          if self.deque.prev is None:
+              # coachable
               return ''
-          curr = curr.prev
-            
-    #   if curr is None:
-    #       self.tail = None
-    #   else:
-      self.tail = curr
-      return curr.val
+              # leetcode
+            #   break
+          self.deque = self.deque.prev
+      return self.deque.val
 
   # Navigates forward N pages in browser history.
   # If there are not N pages ahead, then go as far as you can.
   # After navigating return the URL of the current page.
   def forward(self, N):
-      if self.head is None:
-            return
-      curr = self.head
-      tailend = self.tail
       for i in range( N ):
-          self.add_to_front_of_tail( curr )
-          if curr.next is None:
-              break          
-          curr = curr.next
-              
-    #   if curr is None:
-        #   self.head = None
-    #   else:
-      self.head = curr.next
-      return curr.val
+          if self.deque.next is None:
+              break
+          self.deque = self.deque.next
+      return self.deque.val
 
-  def add_to_front_of_tail(self, val):
-      new_node = Node( val )
-      new_node.prev = self.tail
-      self.tail = new_node
-      
-  def add_to_front_of_front(self, node):
-      temp = self.head
-      new_node = node
-      new_node.next = self.head
-      self.head = new_node
-
-
-  def asList(self):
-    lst = []
-    deux = []
-    head = self.head
-    tail = self.tail
-    while head:
-        lst.append( head.val )
-        head = head.next
-    while tail:
-        deux.append( tail.val )
-        tail = tail.next
-    return [ lst, deux ]
-
-if __name__ == '__main__':
-    browserHistory = BrowserHistory("leetcode.com")
-    print( browserHistory.asList() )
-    print( browserHistory.visit("google.com") )
-
-    print( browserHistory.asList() )
-    print( browserHistory.visit("facebook.com") )
-    print( browserHistory.asList() )
-    print( browserHistory.visit("youtube.com") )
-    print( browserHistory.asList() )
-    print( browserHistory.back(1) )
-    print( browserHistory.asList() )
-    print( browserHistory.back(1) )
-    print( browserHistory.asList() )
-    print( browserHistory.forward(1) )
-    print( browserHistory.asList() )
-    browserHistory.visit("linkedin.com")
-    print( browserHistory.asList() )
-    print( browserHistory.forward(2) )
-    print( browserHistory.asList() )
-    print( browserHistory.back(2) )
-    print( browserHistory.asList() )
-    print( browserHistory.back(7) )
-    print( browserHistory.asList() )
+__name__ = '__main__':
+    history = BrowserHistory()
+    history.go_to("leetcode.com") 
+    history.go_to("google.com")                # You are in "leetcode.com". Visit "google.com"
+    history.go_to("facebook.com")              # You are in "google.com". Visit "facebook.com"
+    history.go_to("youtube.com")               # You are in "facebook.com". Visit "youtube.com"
+    history.go_back()                          # You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+    history.go_back()                          # You are in "facebook.com", move back to "google.com" return "google.com"
+    history.go_forward()                       # You are in "google.com", move forward to "facebook.com" return "facebook.com"
+    history.go_to("linkedin.com")              # You are in "facebook.com". Visit "linkedin.com"
+    history.skip_forward(2)                    # You are in "linkedin.com", you cannot move forward any steps.
+    history.skip_backward(2)                   # You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+    history.skip_backward(7)                   # You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
