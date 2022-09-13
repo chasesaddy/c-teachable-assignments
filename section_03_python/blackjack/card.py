4c4a42b31e49cdd0c66a70fed3daa2b3e7c241c8
Saadh S <atextbooksituation@gmail.com>
2022-09-13 09:57:26 -0400

    Adding OOP basics

--- section_03_python/blackjack/card.py
+++ section_03_python/blackjack/card.py
@@ -2,18 +2,24 @@ class Card:
     # Card constructor
     # The suit and value of a card, should be immutable.
     def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
+        self.__suit = suit
+        self.__value = value
     
     # Returns the suit of the card.
-    def suit(self):
-        return self.suit
+    def get_suit(self):
+        return self.__suit
+
+    def set_suit( self, val ):
+        self.__suit = val
     
     # Returns the value of the card.
-    def value(self):
-        return self.value
+    def get_value(self):
+        return self.__value
+
+    def set_value( self, val ):
+        self.__value = val
       
     # Returns a string representation of Card
     # E.g. "Ace of Spades"
     def __str__(self):
-        return self.suit + ' of ' + self.value
+        return self.get_value() + ' of ' + self.get_suit()
