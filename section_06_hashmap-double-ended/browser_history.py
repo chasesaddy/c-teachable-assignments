d0607c48894762a8f8f03145e4a292eddc73ac35
Saadh S <atextbooksituation@gmail.com>
2022-09-13 09:56:15 -0400

    Fixing up small issues or to look better. Plus the asList print method

--- section_06_hashmap-double-ended/browser_history.py
+++ section_06_hashmap-double-ended/browser_history.py
@@ -7,8 +7,8 @@ class Node:
 class BrowserHistory:
   # Initializes browser hisotry from new tab. 
   # By default the starting URL will be an empty string.
-  def __init__(self, homepage): 
-      self.curr = ''
+  def __init__(self): 
+      self.curr = Node( '' )
   
   # Returns the url of the current page.
   def current_page(self):
@@ -20,7 +20,7 @@ class BrowserHistory:
       tmp = self.curr
       new_node = Node( page )
       # the old/current pointer
-      self.curr.next = new_node  
+      tmp.next = new_node
       # now this is the new_node at pointer
       self.curr = new_node
       # setting new_node's and now the pointer prev
@@ -67,16 +67,16 @@ class BrowserHistory:
           self.curr = self.curr.next
       return self.curr.val
 
-__name__ = '__main__':
-    history = BrowserHistory()
-    history.go_to("leetcode.com") 
-    history.go_to("google.com")                # You are in "leetcode.com". Visit "google.com"
-    history.go_to("facebook.com")              # You are in "google.com". Visit "facebook.com"
-    history.go_to("youtube.com")               # You are in "facebook.com". Visit "youtube.com"
-    history.go_back()                          # You are in "youtube.com", move back to "facebook.com" return "facebook.com"
-    history.go_back()                          # You are in "facebook.com", move back to "google.com" return "google.com"
-    history.go_forward()                       # You are in "google.com", move forward to "facebook.com" return "facebook.com"
-    history.go_to("linkedin.com")              # You are in "facebook.com". Visit "linkedin.com"
-    history.skip_forward(2)                    # You are in "linkedin.com", you cannot move forward any steps.
-    history.skip_backward(2)                   # You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
-    history.skip_backward(7)                   # You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
+  def asList( self ):
+    forward = []
+    back = []
+    curr = self.curr
+    while curr:
+      forward.append( curr.val )
+      curr = curr.next
+    curr = self.curr
+    while curr.prev:
+      curr = curr.prev
+      back.append( curr.val )
+    return [ forward, back ]
+
